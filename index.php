15523e93b005b8207f203ca99422a1798724f98d
Xeoncross <david@xeoncross.com>
2010-12-08 23:21:28 -0600

    Added ruff E_FATAL catching

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ define('AJAX',strtolower(getenv('HTTP_X_REQUESTED_WITH'))==='xmlhttprequest');
 require('functions.php');
 require('bootstrap.php');
 set_error_handler(function($c,$e,$f=0,$l=0){$v=new View('error');$v->e=$e;$v->f=$f;$v->l=$l;echo$v;_log("$e [$f:$l]");});
-set_exception_handler(function($e){$v=new View('exception');$v->e=$e;_log($e->getMessage().' '.$e->getFile());die($v);});
+function exception($e){$v=new View('exception');$v->e=$e;_log($e->getMessage().' '.$e->getFile());die($v);}
+set_exception_handler('exception');
+register_shutdown_function(function(){if($e=error_get_last())exception(new ErrorException($e['message'],$e['type'],0,$e['file'],$e['line']));});
 $c='controller_'.(url(0)?:'home');$m=url(1)?:'index';if(!is_file(p("classes/$c"))||!($c=new$c)||!method_exists($c,$m)){$c=new controller;$m='show_404';}
 call_user_func_array(array($c,$m),array_slice(url(),2));
